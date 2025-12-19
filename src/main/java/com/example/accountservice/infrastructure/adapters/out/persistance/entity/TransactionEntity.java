370366595119c22b809425d5549634230feb4cde
jpajuelo <jpajuelogibaja@gmail.com>
2025-12-19 00:01:11 -0500

    add propiedades Entity

--- src/main/java/com/example/accountservice/infrastructure/adapters/out/persistance/entity/TransactionEntity.java
+++ src/main/java/com/example/accountservice/infrastructure/adapters/out/persistance/entity/TransactionEntity.java
@@ -2,12 +2,21 @@ package com.example.accountservice.infrastructure.adapters.out.persistance.entit
 
 import com.example.accountservice.domain.model.TransactionType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
 @Entity
+@Table(name = "Transaction")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class TransactionEntity {
     @Id
     private UUID transactionId;
